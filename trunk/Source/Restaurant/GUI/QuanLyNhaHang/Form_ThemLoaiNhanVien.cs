cf62588c8d2452708b0bcb49eec50c0350c3b05b
minhnghia20290@gmail.com <minhnghia20290@gmail.com@f105f152-43ed-969b-29ae-013174142892>
2012-05-10 14:12:59 +0000

    10/5

---
+++ trunk/Source/Restaurant/GUI/QuanLyNhaHang/Form_ThemLoaiNhanVien.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DTO;
+using BUS;
+
+namespace GUI.QuanLyNhaHang
+{
+    public partial class Form_ThemLoaiNhanVien : Form
+    {
+        public LoaiNhanVien_DTO loaiNV = new LoaiNhanVien_DTO();
+        public Form_ThemLoaiNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThemLoai_Click(object sender, EventArgs e)
+        {
+            loaiNV.MaLoaiNV = txtMaLoai.Text;
+            loaiNV.TenLoaiNV = txtTenLoaiNV.Text;
+            loaiNV.Luong =long.Parse(txtLuong.Text);
+            if (LoaiNhanVien_BUS.KiemTraMaLoai(txtMaLoai.Text) == 1)
+            {
+                DevExpress.XtraEditors.XtraMessageBox.Show("Mã Loại Nhân viên đã tồn tại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+               
+            }
+            else if (LoaiNhanVien_BUS.KiemTraTenLoai(txtTenLoaiNV.Text) == 1)
+            {
+                DevExpress.XtraEditors.XtraMessageBox.Show("Tên Mã Loại Nhân viên đã tồn tại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+               
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
